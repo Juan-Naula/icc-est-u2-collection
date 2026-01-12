01aa9c91344fc1b590da75ea8baab272be69fc9d
Juan-Naula <juan.miguel.naula.abad@gmail.com>
2026-01-12 12:52:03 -0500

    Sets

---
+++ src/structures/sets/Sets.java
@@ -0,0 +1,41 @@
+package structures.sets;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+    public Set<String> construirHashSet(){
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("A");
+        hashSet.add("B");
+        hashSet.add("C");
+        hashSet.add("D");
+        return hashSet;
+    }   
+    
+    public Set<String> construirLinkedHashSet() {
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("A");
+        linkedHashSet.add("B");
+        linkedHashSet.add("C");
+        linkedHashSet.add("D");
+        linkedHashSet.add("E");
+        linkedHashSet.add("F");
+        return linkedHashSet;
+    }
+
+    public Set<String> construirTreeSet(){
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("D");
+        treeSet.add("A");
+        treeSet.add("E");
+        treeSet.add("I");
+        treeSet.add("O");
+        treeSet.add("B");
+        treeSet.add("a");
+        return treeSet;
+    }
+    
+}
